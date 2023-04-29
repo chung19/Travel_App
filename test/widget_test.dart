7dc05c3ad6e5c733feb3d80353348279d59c552d
chung19 <59258408+chung141219@users.noreply.github.com>
2023-04-29 15:46:51 +0700

    update intro at view on board

---
+++ test/widget_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class fff extends StatefulWidget {
+  const fff({Key? key}) : super(key: key);
+
+  @override
+  State<fff> createState() => _fffState();
+}
+
+class _fffState extends State<fff> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
